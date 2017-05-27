f9708aeb1b7d0b88a532ea5737d8e25e8300c87a
Jonas Wolf <jonas@Jonass-Air.fritz.box>
2017-05-27 20:47:29 +0200

    Restructured

--- src/rotate.d
+++
@@ -1,8 +0,0 @@
-module jw.util.rotate;
-
-/// Tempalte to rotate bits of unsigned integers.
-T rotate_left(T, immutable int num)(T value)
- if (__traits(isUnsigned, T) && // Rotate left is only available for unsigned types.
-    (num < T.sizeof*8)) { // Number of bits to rotate must be less than bits of T.
-	    return (value << num) | (value >> (T.sizeof*8-num));
-}
